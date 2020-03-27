ba03feaa9adaf8fe9fc2cac06ea9e7108e8d5aac
aldair05 <aldair.uaem@gmail.com>
2020-03-26 19:16:15 -0600

    agrego Php

--- Enviar.php
+++ Enviar.php
@@ -27,7 +27,7 @@
             $mail->Host = 'smtp.gmail.com';                       // servicio de correo  que se va utilizar 
             $mail->SMTPAuth = true;                               // 
             $mail->Username = 'aldair.uaem@gmail.com';          // correo desde donde se va enviar el correo 
-            $mail->Password = 'kiderzom05.2017';                      // contraseña 
+            $mail->Password = '';                      // contraseña 
             $mail->SMTPSecure = 'tls';                            // 
             $mail->Port = 587;                                    // 
             //Recipients
@@ -70,8 +70,7 @@
                             </table>
                 </tr>
             </table>
-        </body>
-            
+        </body>   
     </head>
 <html>";
             $exito = $mail->Send();
