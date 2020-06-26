7245e0623b83cbd98760f189f4f02e93e24135d8
aldair05 <aldair.uaem@gmail.com>
2020-06-25 23:36:10 -0500

    Actualizacion Iconos

--- js/scrip.js
+++ js/scrip.js
@@ -1,27 +1,26 @@
 
-/* Inicio de funcion de menu Navegador*/
-$(window).scroll(function() {
-        if ($("#menu").offset().top > 40) {
-            $("#menu").addClass("bg-dark");
-        } else {
-            $("#menu").removeClass("bg-dark");
-        }
+//Menu Responsive 
+$(function(){
+    $('.menu-movil').on('click',function(){
+        console.log('diste click');
+        $('.navegacion-principal').slideToggle();
+    });
 });
 
-/*Fin de funcion de menu Navegador*/
+
 
 
 /* Inicio de Funcion de filtrado Imagenes*/
-(function(){
-    $(document).ready(function(){
-        $(".btn-menu").click(function(){
+(function () {
+    $(document).ready(function () {
+        $(".btn-menu").click(function () {
             var filtro = $(this).attr("data-filter");
 
-            if(filtro == "todos"){
+            if (filtro == "todos") {
                 $(".imagen").show(500);
-            }else{
-                $(".imagen").not("."+ filtro).hide(500);
-                $(".imagen").filter("."+ filtro).show(500);
+            } else {
+                $(".imagen").not("." + filtro).hide(500);
+                $(".imagen").filter("." + filtro).show(500);
             }
         });
     });
@@ -32,21 +31,21 @@ $(window).scroll(function() {
 /* FIn de Funcion de filtrado Imagenes*/
 
 /*Funcion de transicion enlaces*/
-$('li a').click(function(e){
+$('li a').click(function (e) {
     e.preventDefault();
-    var strAncla=$(this).attr('href');
-    $('body,html').stop(true,true).animate({
+    var strAncla = $(this).attr('href');
+    $('body,html').stop(true, true).animate({
         scrollTop: $(strAncla).offset().top
-    },1000);
+    }, 1000);
 
 });
 
-$('div .enlace').click(function(e){
+$('div .enlace').click(function (e) {
     e.preventDefault();
-    var strAncla=$(this).attr('href');
-    $('body,html').stop(true,true).animate({
+    var strAncla = $(this).attr('href');
+    $('body,html').stop(true, true).animate({
         scrollTop: $(strAncla).offset().top
-    },1000);
+    }, 1000);
 });
 
 /*Fin de Funcion de transicion enlaces*/
@@ -69,12 +68,12 @@ window.addEventListener('scroll', mostrarScrollAcerca);
 //Funciones
 function mostrarScrollAcerca() {
     let scrollTop = document.documentElement.scrollTop;
-    for (let i = 0 ; i < animado.length; i++){
-            let alturaAnimado = animado[i].offsetTop;
-            if (alturaAnimado - 900 < scrollTop) {
-                // animado[i].style.opacity = 1;
-                animado[i].classList.add('fadeInDown');
-            }
+    for (let i = 0; i < animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+        if (alturaAnimado - 900 < scrollTop) {
+            // animado[i].style.opacity = 1;
+            animado[i].classList.add('fadeInDown');
+        }
     }
 }
 
@@ -91,49 +90,49 @@ const btnReset = document.getElementById('reset');
 //EventListener 
 eventListener();
 
-function eventListener(){
+function eventListener() {
     //Inicio de la aplicacion y desabilita el boton enviar
-    document.addEventListener('DOMContentLoaded',inicioApp);
+    document.addEventListener('DOMContentLoaded', inicioApp);
 
     //Campos del formulario 
-    nombre.addEventListener('blur',validarCampo);
-    email.addEventListener('blur',validarCampo);
-    telefono.addEventListener('blur',validarCampo);
-    comentario.addEventListener('blur',validarCampo);
+    nombre.addEventListener('blur', validarCampo);
+    email.addEventListener('blur', validarCampo);
+    telefono.addEventListener('blur', validarCampo);
+    comentario.addEventListener('blur', validarCampo);
 
     //Boton de enviar en el submit 
-    btnEnviar.addEventListener('click',enviarEmail);
+    btnEnviar.addEventListener('click', enviarEmail);
 }
 //Funciones
 
-function inicioApp(){
+function inicioApp() {
     //deshabilita el boton enviar 
-    btnEnviar.disabled = true; 
+    btnEnviar.disabled = true;
 
 }
 
 //valida que el campo tenga algo escrito 
 function validarCampo() {
-    
+
     //Se valida la logitud del texto y que no este vacio 
     validarLongitud(this);
 
-    
+
     //Validar unicamente el email 
-    if (this.type === 'email'){
+    if (this.type === 'email') {
         validarEmail(this);
     }
 
     let errores = document.querySelectorAll('.error');
-    if(nombre.value !== '' && email.value !== '' && telefono.value !== '' && comentario.value !== ''){
-        if(errores.length === 0){
+    if (nombre.value !== '' && email.value !== '' && telefono.value !== '' && comentario.value !== '') {
+        if (errores.length === 0) {
             btnEnviar.disabled = false;
-            
-        }    
+
+        }
     }
 }
 
-function enviarEmail(e){
+function enviarEmail(e) {
     //spinner al presionar Enviar
     const spinnerGif = document.querySelector('#spinner');
     spinnerGif.style.display = 'block';
@@ -142,46 +141,64 @@ function enviarEmail(e){
     const enviado = document.createElement('img');
     enviado.src = 'imagenes/mail.gif';
     enviado.style.width = '150px';
-    enviado.style.margin = '0 auto'; 
+    enviado.style.margin = '0 auto';
     enviado.style.display = 'block';
-    
+
 
     //Ocultar Spinner y mostrar gif de enviado 
-    setTimeout(function() {
-            spinnerGif.style.display = 'none';
-            document.querySelector('#loaders').appendChild( enviado );
-            setTimeout(function(){
-                enviado.remove();
-                formularioEnviar.reset();
-                
-            },5000);
-    },3000);
-    
-   
+    setTimeout(function () {
+        spinnerGif.style.display = 'none';
+        document.querySelector('#loaders').appendChild(enviado);
+        setTimeout(function () {
+            enviado.remove();
+            formularioEnviar.reset();
+
+        }, 5000);
+    }, 3000);
+
+
 }
 
 
 
 //Verifica la longitu del texto en los campos
-function validarLongitud(campo){      
-    if(campo.value.length > 0){
+function validarLongitud(campo) {
+    if (campo.value.length > 0) {
         campo.style.borderColor = 'green';
         campo.classList.remove('error');
-    }else{
+    } else {
         campo.style.borderColor = 'red';
         campo.classList.add('error');
     }
 }
 
-function validarEmail(campo){
+function validarEmail(campo) {
     const mensaje = campo.value;
-    
-    if(mensaje.indexOf('@') !== -1){
+
+    if (mensaje.indexOf('@') !== -1) {
         campo.style.borderColor = 'green';
         campo.classList.remove('error');
-    }else{
+    } else {
         campo.style.borderColor = 'red';
         campo.classList.add('error');
     }
 }
+
+//Mapa
+
+
+$(window).scroll(function (){
+
+    var map = L.map('mapa').setView([19.285818, -98.93653], 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.marker([19.285818, -98.93653]).addTo(map)
+        .bindPopup('New Print')
+        .openPopup();
+
+        
+});
 
